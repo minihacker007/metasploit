416843ee9bf7a2ea633d8f4702bdf4e110a0a32b
techzofvikas <87221494+techzofvikas@users.noreply.github.com>
2021-07-10 12:22:45 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 apt update
 apt upgrade
 apt install wget curl openssh git -y 
-#apt install python
-#apt install python2
-apt install ncurses-utils
+apt install python -y
+apt install python2 -y
+apt install ncurses-utils -y
 wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh
 chmod +x metasploit.sh
 bash metasploit.sh
